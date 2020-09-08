b1d8dae5c4354d749b4ac56ecf6e28e7fb1ef8a5
Wahyu Ramadan <wramadan122@yahoo.com>
2020-09-08 23:31:22 +0700

    add post(/data)

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 
 const app = express()
 
+const bodyParser = require('body-parser')
+
 const port = 8080
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/home', (req, res) => {
 	res.send('Welcome to my first backend!')
 })
+
+app.post('/data', (req, res) => {
+	console.log('Post dari body : ', req.body)
+})
+
 
 app.listen(port, () => {
 	console.log('Port sedang berjalan')
