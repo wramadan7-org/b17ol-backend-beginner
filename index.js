f862291912144853cba52cb25f7e4cb4423796e3
Wahyu Ramadan <wramadan122@yahoo.com>
2020-09-09 09:39:44 +0700

    add patch (/profile)

--- index.js
+++ index.js
@@ -17,8 +17,28 @@ app.post('/data', (req, res) => {
 	console.log('Post dari body : ', req.body)
 })
 
-app.post('/profile', (req, res) => {
-	res.send(req.body)
+// app.post('/profile', (req, res) => {
+// 	res.send(req.body)
+// 	console.log(req.body)
+// })
+
+app.patch('/profile', (req, res) => {
+	
+	let dataRes = {
+		name: req.body.name,
+		batch: req.body.batch,
+		email: req.body.email
+	}
+
+	let data = {
+		name: 'Jane',
+		batch: '17.2',
+		email: 'janeoud@mail.com'
+	} 
+
+	let filter = {...dataRes, ...data}
+	res.send(filter)
+	console.log(filter)
 })
 
 
